31836c861139049cd32e352d1f0816e08c66363a
ttrangih <22521514@gm.uit.edu.vn>
2025-12-04 22:45:37 +0700

    update project

--- src/components/BottomNavbar.jsx
+++ src/components/BottomNavbar.jsx
@@ -4,7 +4,29 @@ import { faHouse, faUserFriends, faPlus, faInbox, fa7,faUser } from '@fortawesom
 
 function BottomNavbar() {
   return (
-
+    <div className='bottom-navbar'>
+      <div className='nav-item'>
+        <FontAwesomeIcon icon={faHouse} className="icon active"/>
+        <span className='item-name active'>Home</span>
+      </div>
+      <div className='nav-item'>
+        <FontAwesomeIcon icon={faUserFriends} className="icon"/>
+        <span className='item-name'>Friends</span>
+      </div>
+      <div className='nav-item'>
+        <FontAwesomeIcon icon={faPlus} className="icon plus"/>
+        <span className='item-name'>Create</span>
+      </div>
+      <div className='nav-item'>
+        <FontAwesomeIcon icon={fa7} className="notification"/>
+        <FontAwesomeIcon icon={faInbox} className='icon'/>
+        <span className='item-name'>Inbox</span>
+      </div>
+      <div className='nav-item'>
+        <FontAwesomeIcon icon={faUser} className="icon"/>
+        <span className='item-name'>Profile</span>
+      </div>
+    </div>
     
   );
 }
--- src/components/TopNavbar.jsx
+++ src/components/TopNavbar.jsx
@@ -1,10 +1,15 @@
+//topnavbar
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTv, faSearch } from '@fortawesome/free-solid-svg-icons';
 
 const TopNavbar = () => {
   return (
-
+      <div className='top-navbar'>
+        <FontAwesomeIcon icon={faTv} className='icon'/>
+        <h2>Following   | <span>For you</span></h2>
+        <FontAwesomeIcon icon={faSearch} className='icon'/>
+      </div>
   );
 };
 
